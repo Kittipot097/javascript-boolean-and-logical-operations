6b78da01b0f3c3de0623cd41e6890aa70ca00d6b
jerpuen <kittipot.kulpinit@gmail.com>
2024-08-20 17:20:50 +0700

    fix: ex2

--- ex-2.js
+++ ex-2.js
@@ -1,16 +1,12 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000 = true; // ถ้ามากกว่า 4000
-let isWeekday = true; // ถ้าเป็น จันทร์ ถึง ศุกร์
-let hasBoughtProductFromITCategory = true; // สินค้า it
-let hasAttendedDiscountEvent = true; // เคยเข้าร่วมงานลดราคา
-let isPlatinum = true; // สมาชิก platinum
+let lastMonthPaidMoreThan4000 = true;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = false;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = false; 
 
-let hasPromotion = true; // ได้รับส่วนลด
-
-console.log(hasPromotion);
-
-hasPromotion = lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && hasAttendedDiscountEvent && !isPlatinum; // john
+let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent) || isPlatinum; // john
 
 console.log(hasPromotion);
